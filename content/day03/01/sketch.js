25d4b0470a2b1afd799d4f18789f23b6e59162ea
pis00 <pisano.luca37@gmail.com>
2025-10-30 15:23:48 +0100

    try

--- content/day03/01/sketch.js
+++ content/day03/01/sketch.js
@@ -42,7 +42,7 @@ function make2D(rows, cols, val = C_WHITE) {
 }
 
 function setup() {
-  createCanvas(600, 450);
+  createCanvas(400, 450);
   calculateCellSize();
 
   currentMask = make2D(GRID_ROWS, GRID_COLS, C_WHITE);
